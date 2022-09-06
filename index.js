ad82f3482e988eb1bbe2c794e3654f0b237d98b0
dbtop <berdaddario@gmail.com>
2022-09-06 19:15:25 -0300

    do it better

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from 'express' //1°
+import dotenv from 'dotenv' //2°
+import cors from 'cors' //3°
+import conectarDB  from './config/db.js'; //3°
+import userRoutes from './routes/userRoutes.js';
+import projectRoutes from './routes/projectRoutes.js'
+import taskRoutes from './routes/taskRoutes.js'
+
+
+const app = express(); 
+app.use(cors()); 
+
+app.use(express.json()); 
+
+dotenv.config(); 
+
+conectarDB(); 
+
+//Router
+app.use("/api/users", userRoutes); //4°
+app.use("/api/projects", projectRoutes); //4°
+app.use("/api/task", taskRoutes); //4°
+
+
+const PORT = process.env.PORT || 3000; 
+
+const server = app.listen(PORT, () => {
+     console.log(`MERN app listening on port ${PORT}!`);
+    });
+
+//socket.io
+import { Server } from "socket.io"
+
+const io = new Server(server,{
+    pingTimeout: 60000,
+    pingInterval: 25000,
+    cors:{
+        origin: "*"
+    },
+})
+
+io.on("connection", (socket)=> {
+    console.log("connected to socket.io")
+    //Events socket io
+    socket.on("open project", (projectId)=>{
+        socket.join(projectId);
+    });
+
+
+    socket.on("add task" ,(task) =>{       
+        socket.to(task.project).emit("task added", task);
+    });
+
+    socket.on("delete task", (task)=>{
+        socket.to(task.project).emit("task deleted", task);
+    })
+
+    socket.on("update task", (task) =>{
+        const project = task.project._id;
+        socket.to(project).emit("updated task", task);
+    });
+
+    socket.on("complete task", (task) => {
+        const project = task.project;
+        socket.to(project).emit("completed task", task)
+    })
+
+});
+
